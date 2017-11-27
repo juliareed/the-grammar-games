29576d352e10af3ac4be3ff448d26d7f23fa7378
Michael Yawanis <michael@dhcp-10-102-253-197.wireless.northwestern.private>
2017-11-27 13:44:53 -0600

    basic folder structure

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/the-grammar-games",
+  {
+    useMongoClient: true
+  }
+);
+
+//Setup Mongoose DB
+const db = require("./models")
+const { Article } = db;
