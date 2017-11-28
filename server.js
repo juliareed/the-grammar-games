89a150e1c6d196e3094e6bf89d3fdc73b37ab5cd
Michael Yawanis <michael@dhcp-10-102-253-197.wireless.northwestern.private>
2017-11-28 13:59:13 -0600

    updated port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Configure body parser for AJAX requests
