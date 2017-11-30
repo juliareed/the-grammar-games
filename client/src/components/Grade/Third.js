8b3c21d961b23f6f098c848ea81efb0a49a29209
Julia Reed <msjuliareed@gmail.com>
2017-11-30 13:40:38 -0600

    routes

---
+++ client/src/components/Grade/Third.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Navbar from "../Navbar/Navbar"
+import Footer from "../Footer/Footer"
+// import Quiz from "./Quiz/Quiz"
+import "../main.css"
+// import Main from "../Main"
+
+class Third extends Component {
+  
+    render() {
+  
+      return (
+        <div className="main-container">
+          <div className="container">
+          <Navbar />
+          <br/>
+          <p>
+            <button className="btn btn-primary btn-lg">Three ways to end a sentence</button>&nbsp;&nbsp;&nbsp;&nbsp;
+            <button className="btn btn-primary btn-lg">Common and proper nouns</button>&nbsp;&nbsp;&nbsp;&nbsp;
+            <button className="btn btn-primary btn-lg">Personal pronouns</button>
+          </p>
+          <br/>
+          <hr />
+          <Footer />
+          </div>
+        </div>
+      );
+    }
+  };
+  
+  // Export the module back to the route
+  export default Third;
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./Navbar/Navbar"
 import Video from "./Video/Video"
 import Footer from "./Footer/Footer"
+import Third from "./Grade/Third.js"
+import Fourth from "./Grade/Fourth.js"
+import Fifth from "./Grade/Fifth.js"
+import Sixth from "./Grade/Sixth.js"
 // import Quiz from "./Quiz/Quiz"
 import "./main.css"
 
@@ -10,15 +15,26 @@ class Main extends Component {
     render() {
   
       return (
+        <div>
+          <div>
+            <Router>
+               <div>
+                  <Route path="/Third" component={Third} />
+                  <Route path="/Fourth" component={Fourth} />
+                  <Route path="/Fifth" component={Fifth} />
+                  <Route path="/Sixth" component={Sixth} />
+              </div>
+            </Router>
+
         <div className="main-container">
           <div className="container">
           <Navbar />
           <br/>
           <p>
-            <a href="/../Grade/3rdgrade.js" className="btn btn-primary btn-lg">3rd Grade</a>&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="/../Grade/4thgrade.js" className="btn btn-primary btn-lg">4th Grade</a>&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="/../Grade/5thgrade.js" className="btn btn-primary btn-lg">5th Grade</a>&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="/../Grade/6thgrade.js" className="btn btn-primary btn-lg">6th Grade</a>
+            <button className="btn btn-primary btn-lg">3rd Grade</button>&nbsp;&nbsp;&nbsp;&nbsp;
+            <button className="btn btn-primary btn-lg">4th Grade</button>&nbsp;&nbsp;&nbsp;&nbsp;
+            <button className="btn btn-primary btn-lg">5th Grade</button>&nbsp;&nbsp;&nbsp;&nbsp;
+            <button className="btn btn-primary btn-lg">6th Grade</button>
           </p>
           <Video />
           <br/>
@@ -26,6 +42,8 @@ class Main extends Component {
           <Footer />
           </div>
         </div>
+       </div>
+       </div>
       );
     }
   };
