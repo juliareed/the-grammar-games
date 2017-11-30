512e3888ce25d6a9c627aa1fec3811401ca09ea2
Michael Yawanis <michael@dhcp-10-102-253-197.wireless.northwestern.private>
2017-11-30 11:17:57 -0600

    updated components

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { Route, Router, IndexRoute, hashHistory } from 'react-router';
 import Navbar from "./Navbar/Navbar"
 import Video from "./Video/Video"
 import Footer from "./Footer/Footer"
-// import Quiz from "./Quiz/Quiz"
+import Answers from "./Answers/Answers"
+import Popup from "./Popup/Popup"
+import Quiz from "./Quiz/Quiz"
+import data from '../data/data';
 import "./main.css"
 
 class Main extends Component {
@@ -16,6 +20,7 @@ class Main extends Component {
           <br/>
           <Video />
           <br/>
+          <Quiz />
           <hr />
           <Footer />
           </div>
