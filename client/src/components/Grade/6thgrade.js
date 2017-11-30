d0b18df8e3623c55f5d771a347c477930624d1d1
Julia Reed <msjuliareed@gmail.com>
2017-11-30 11:15:41 -0600

    added grade component

--- client/src/components/Grade/6thgrade.js
+++ client/src/components/Grade/6thgrade.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Navbar from "./Navbar/Navbar"
-import Video from "./Video/Video"
 import Footer from "./Footer/Footer"
 // import Quiz from "./Quiz/Quiz"
 import "./main.css"
@@ -15,12 +14,11 @@ class Main extends Component {
           <Navbar />
           <br/>
           <p>
-            <a href="/../Grade/3rdgrade.js" className="btn btn-primary btn-lg">3rd Grade</a>&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="/../Grade/4thgrade.js" className="btn btn-primary btn-lg">4th Grade</a>&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="/../Grade/5thgrade.js" className="btn btn-primary btn-lg">5th Grade</a>&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="/../Grade/6thgrade.js" className="btn btn-primary btn-lg">6th Grade</a>
+            <a href="#/quiz_add" className="btn btn-primary btn-lg">3rd Grade</a>&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="#/quiz_play" className="btn btn-primary btn-lg">4th Grade</a>&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="#/quiz_add" className="btn btn-primary btn-lg">5th Grade</a>&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="#/quiz_add" className="btn btn-primary btn-lg">6th Grade</a>
           </p>
-          <Video />
           <br/>
           <hr />
           <Footer />
